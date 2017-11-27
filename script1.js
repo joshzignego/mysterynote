612d636ec206457731e75520e7dc8b23c044372e
Josh Zignego <ziggybadger@gmail.com>
2017-11-27 08:14:12 -0600

    enter name

---
+++ script1.js
@@ -0,0 +1,7 @@
+var person = prompt("Please enter your name", "");
+
+if (person == null || person == "") {
+    txt = "User cancelled the prompt.";
+} else {
+    txt = "Hello " + person + "! How are you today?";
+}
