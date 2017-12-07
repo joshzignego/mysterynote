121664fed6bf1c55a14e30e6b969b280e693b11a
Josh Zignego <ziggybadger@gmail.com>
2017-12-07 13:41:15 -0600

    try

--- website/getNotes.php
+++ website/getNotes.php
@@ -3,15 +3,14 @@
     //$content = json_decode(file_get_contents($file), true);
     //echo json_encode($content);
 
-/*
+
 $host = "ec2-54-243-211-197.compute-1.amazonaws.com";
 $dbname = "d2h07jacfl1u2v";
 $user = "fbzwtonwoufowv";
 $password = "5e064691dd5227cee61ada3cc843b64ca68eaf3fe99c5bb8c9a9df46aa2b47cd";
 $port = "5432";
-$databaseURL =  getenv("DATABASE_URL");
+//$databaseURL =  getenv("DATABASE_URL");
 //$dbconn = pg_connect();
-*/
 
 
 
@@ -19,7 +18,8 @@ $databaseURL =  getenv("DATABASE_URL");
 
 
 
-$dbconn = pg_connect("host=ec2-54-243-211-197.compute-1.amazonaws.com port=5432 dbname=d2h07jacfl1u2v user=fbzwtonwoufowv password=5e064691dd5227cee61ada3cc843b64ca68eaf3fe99c5bb8c9a9df46aa2b47cd");
+
+$dbconn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 $sql = "create table lorem (id int not null, foo varchar(15), primary key (id));";
 $r = pg_query($dbconn, $sql);
 
